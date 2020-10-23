e6b590545b6ccec1ab2939860d91ab1af4cbb365
Chloe Langford <chloe@gene.co.uk>
2020-10-23 08:30:29 +0100

    origin/main Initial commit

---
+++ pages/posts/first-post.js
@@ -0,0 +1,30 @@
+import Link from 'next/link'
+import Head from 'next/head'
+import Layout from '../../components/layout'
+
+export default function FirstPost() {
+    return (
+
+        <Layout>
+        <div className="container">
+            <Head>
+                <title>Fashion Swipe</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <h1>First Post</h1>
+            <h2>
+                <Link href="/">
+                    <a>Back to home</a>
+                </Link>
+            </h2>
+
+            <style jsx>{`
+                .container {
+                    background-color: #f3f3f3;
+                }
+            `}</style>
+
+        </div>
+        </Layout>
+    )
+}
